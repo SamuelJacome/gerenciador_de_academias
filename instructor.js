9ed712e3852543bb7cf409acd9c5cdaaf6383a7a
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-16 17:29:09 -0300

    salvando os dados em um arquivo json

--- instructor.js
+++ instructor.js
@@ -1,3 +1,7 @@
+const fs = require('fs')
+const data = require("./data.json")
+
+
 //Create
 
 exports.post = function(req, res){
@@ -10,7 +14,30 @@ exports.post = function(req, res){
             return res.send("Please, fill all fields ")
         }
     }
-    return res.send(req.body)
+
+    req.body.birth = Date.parse(req.body.birth)
+    req.body.created_at = Date.now()
+    req.body.id = Number(data.instructors.length+1)
+    data.instructors.push({ 
+        avatar_url,
+        birth,
+        created_at,
+        id,
+        gender,
+        services,
+        name    
+    }) 
+
+
+    const{avatar_url, birth, created_at, id, services, gender} = req.body
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("write file error!")
+        
+        return res.redirect("/instructors")
+
+    })
+    // return res.send(req.body)
 }
 
 
