55ea9c8722e4e8583674a51b9f69d70facb554d6
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-20 11:56:12 -0300

    show ok, create ok

--- instructor.js
+++ instructor.js
@@ -18,7 +18,7 @@ exports.show = function(req, res){
         age: age(foundInstructor.birth),
     
         services: foundInstructor.services.split(","),
-        created_at: Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
     }
     return res.render("instructors/show", { instructor })
 
