4b3072648069416186bde2ed817bd68ac1ea87e5
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-20 09:24:59 -0300

    Atualização instructor.js(Lógica de Idade)

--- instructor.js
+++ instructor.js
@@ -1,6 +1,45 @@
 const fs = require('fs')
 const data = require("./data.json")
 
+//show
+
+exports.show = function(req, res){
+    const{ id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return id == instructor.id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+
+
+    function age(timestamp){
+        const today = new Date()
+        const birthDate = new Date(timestamp)
+
+        let age = today.getFullYear() - birthDate.getFullYear()
+        const month = today.getMonth() - birthDate.getMonth()
+       
+        if( month < 0 || month == 0 && today.getDate() <=  birthDate.getDate()){
+            age = age-1
+        }
+        return age
+    }
+    const instructor ={
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+    
+        services: foundInstructor.services.split(","),
+        created_at: "",
+    }
+    return res.render("instructors/show", { instructor })
+
+    
+}
+
+
+
+
 
 //Create
 
@@ -14,22 +53,24 @@ exports.post = function(req, res){
             return res.send("Please, fill all fields ")
         }
     }
+    let{ avatar_url, birth, services, gender, name } = req.body
+    birth = Date.parse(req.body.birth)
+    const created_at = Date.now()
+    const id = Number(data.instructors.length+1)
 
-    req.body.birth = Date.parse(req.body.birth)
-    req.body.created_at = Date.now()
-    req.body.id = Number(data.instructors.length+1)
     data.instructors.push({ 
-        avatar_url,
-        birth,
-        created_at,
         id,
+        avatar_url,
+        name,
+        birth,
         gender,
         services,
-        name    
+        created_at
+            
     }) 
 
 
-    const{ avatar_url, birth, created_at, id, services, gender } = req.body
+ 
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
         if (err) return res.send("write file error!")
@@ -41,7 +82,6 @@ exports.post = function(req, res){
 }
 
 
-//update
 
 
 //delete
