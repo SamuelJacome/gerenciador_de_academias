ed23c52cbc544980fabfe4eb13830295378c5d1d
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-22 19:07:55 -0300

    Rota Edit corrigida

--- instructor.js
+++ instructor.js
@@ -1,29 +1,7 @@
 const fs = require('fs')
 const data = require("./data.json")
 const {age} = require("./utils")
-//show
 
-exports.show = function(req, res){
-    const{ id } = req.params
-
-    const foundInstructor = data.instructors.find(function(instructor){
-        return id == instructor.id
-    })
-
-    if (!foundInstructor) return res.send("Instructor not found")
-
-
-    const instructor ={
-        ...foundInstructor,
-        age: age(foundInstructor.birth),
-    
-        services: foundInstructor.services.split(","),
-        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
-    }
-    return res.render("instructors/show", { instructor })
-
-    
-}
 //Create
 
 exports.post = function(req, res){
@@ -64,6 +42,42 @@ exports.post = function(req, res){
     // return res.send(req.body)
 }
 
+//Edit
+
+exports.edit = function(req, res){
+    const{ id } = req.params
+    const foundInstructor = data.instructors.find(function(instructor){
+        return id == instructor.id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+
+    return res.render('instructors/edit', { instructor : foundInstructor })
+}
+
+//Show
+
+exports.show = function(req, res){
+    const{ id } = req.params
+
+    const foundInstructor = data.instructors.find(function(instructor){
+        return id == instructor.id
+    })
+
+    if (!foundInstructor) return res.send("Instructor not found")
+
+
+    const instructor ={
+        ...foundInstructor,
+        age: age(foundInstructor.birth),
+    
+        services: foundInstructor.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundInstructor.created_at),
+    }
+    return res.render("instructors/show", { instructor })
+
+    
+}
 
 
 
