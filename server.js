5feb8a2b610d9e1ddbedbb1ca7dda1a023906adb
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-23 18:04:27 -0300

    att

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 const routes = require("./routes")
 const server = express()
+const methodOverride =  require('method-override')
 
 server.use(express.urlencoded({ extended: true}))
 server.use(express.static('public'))
 server.use(routes)
+server.use(methodOverride('_method'))
 
 server.set("view engine", "njk")
 
@@ -15,19 +17,6 @@ nunjucks.configure("views", {
     noCache: true
 })
 
-server.get("/", function(req, res){
-    const about = {
-        avata_url: "https://avatars0.githubusercontent.com/u/39867221?s=400&v=4",
-        name: "Samuel Jácome",
-        role:"Desenvolvedor na empresa 'Não sei o nome ainda'",
-        descripton:'Programador Full-Stack, Estudante da <a href="https://ulbra-to.br" target="_blank">Ulbra</a> focado em aprender as melhores práticas. ',
-        links: [
-            {name: "Github", url: "https://github.com/samueljacome/"},
-            {name: "Linkedin", url: "https://www.linkedin.com/in/samuel-j%C3%A1come-a44bb9157/"}
-        ]
-    }
-    return res.render("about", {about: about})
-} )
 
 server.listen(5000, function(){
     console.log("server is running")
