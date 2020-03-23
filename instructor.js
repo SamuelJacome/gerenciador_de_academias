1c5920a459e9b33a8200d91b1228fe8fc3a33ab5
SamuelJacome <samuelvictor@rede.ulbra.br>
2020-03-23 17:19:36 -0300

    att

--- instructor.js
+++ instructor.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require("./data.json")
-const {age} = require("./utils")
+const {age, date} = require("./utils")
 
 //Create
 
@@ -52,8 +52,13 @@ exports.edit = function(req, res){
 
     if (!foundInstructor) return res.send("Instructor not found")
 
-    date(foundInstructor.birth)
-    return res.render('instructors/edit', { instructor : foundInstructor })
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+  
+    return res.render('instructors/edit', { instructor })
 }
 
 //Show
